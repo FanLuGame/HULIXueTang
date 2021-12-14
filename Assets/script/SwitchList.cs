b2a7c238e2e85865f59ddde0ab9e1d8b4198fbea
qsgy <961390320@qq.com>
2021-12-14 16:48:39 +0800

    first
    完成录音界面

---
+++ Assets/script/SwitchList.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchList : MonoBehaviour
+{
+    public int index = 0;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Fresh();
+    }
+
+
+    void Fresh()
+    {
+        if (transform.childCount == 0) return;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i == index)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void Previous()
+    {
+        if (index - 1 >= 0) index--;
+        Fresh();
+    }
+    public void Next()
+    {
+        if (index + 1 < transform.childCount) index++;
+        Fresh();
+    }
+}
