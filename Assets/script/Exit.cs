f74f075f23d13569edbf265bfb69e0633c5e6291
qsgy <961390320@qq.com>
2021-12-15 23:03:56 +0800

    换了3个界面UI

--- Assets/script/Exit.cs
+++ Assets/script/Exit.cs
@@ -14,8 +14,5 @@ public class Exit : MonoBehaviour
 #endif
     }
 
-    public void ReloadGame(string scn)
-    {
-        SceneManager.LoadScene(scn);
-    }
+
 }
---
+++ Assets/script/FuncHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FuncHelper : MonoBehaviour
+{
+    public void ReloadGame(string scn)
+    {
+        SceneManager.LoadScene(scn);
+    }
+}
