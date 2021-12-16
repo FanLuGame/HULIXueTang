62e75c5e42d87d0737a3f443cac000ca534da788
qsgy <961390320@qq.com>
2021-12-16 21:33:42 +0800

    替换完成3个界面。添加得分脚本中

--- Assets/script/SoundHelp.cs
+++ Assets/script/SoundHelp.cs
@@ -6,6 +6,7 @@ public class SoundHelp : MonoBehaviour
 {
     public AudioClip pao;
     public AudioClip wei;
+    public AudioClip didi;
     public void PlaySound(float vo)
     {
         AudioSource.PlayClipAtPoint(pao, FindObjectOfType<AudioListener>().transform.position, vo);
@@ -14,6 +15,11 @@ public class SoundHelp : MonoBehaviour
     public void PlaySound_w(float vo)
     {
         AudioSource.PlayClipAtPoint(wei, FindObjectOfType<AudioListener>().transform.position, vo);
+
+    }
+    public void PlaySound_di()
+    {
+        AudioSource.PlayClipAtPoint(didi, FindObjectOfType<AudioListener>().transform.position, 1);
 
     }
 }
---
+++ Assets/script/Topics/LuYinTopic.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuYinTopic : Topic
+{
+    public override int Score()
+    {
+        if (GetComponent<MicrophoneManager>().audioClip)
+            return fullscore;
+        return 0;
+    }
+
+}
